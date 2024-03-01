c4826b2269cba094344612aef5e46f923482cd53
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-01 22:35:08 +0100

    form with empty values

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -10,3 +10,41 @@ export interface IFrontendEmployee {
 export interface IBackendEmployee extends IFrontendEmployee {
 	id: number;
 }
+
+export interface IFormInfoField {
+	idCode: string;
+	label: string;
+	value: string;
+	isRequired: boolean;
+	isValid: boolean;
+}
+
+export interface IFormInfo {
+	fields: IFormInfoField[];
+}
+
+export const initialFormInfo = {
+	fields: [
+		{
+			idCode: "firstName",
+			label: "First Name",
+			value: "",
+			isRequired: true,
+			isValid: false,
+		},
+		{
+			idCode: "lastName",
+			label: "Last Name",
+			value: "",
+			isRequired: true,
+			isValid: false,
+		},
+		{
+			idCode: "age",
+			label: "Age",
+			value: "",
+			isRequired: true,
+			isValid: false,
+		},
+	],
+};
--- src/pages/PageStateForm.tsx
+++ src/pages/PageStateForm.tsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import { IFormInfo, initialFormInfo } from "../interfaces";
+
 export const PageStateForm = () => {
+	const [formInfo, setformInfo] = useState<IFormInfo>(initialFormInfo);
+
 	return (
-		<p>This is the state form page.</p>
-	)
-}
+		<form>
+			<fieldset className="border border-slate-500 p-4 rounded max-w-[25rem]">
+				<legend>New Member</legend>
+
+				<div className="mb-4 flex gap-2">
+					<label className="w-40" htmlFor="firstName">
+						First Name:
+					</label>
+					<input
+						value={formInfo.fields.find(m => m.idCode === 'firstName')?.value}
+						type="text"
+						id="firstName"
+					/>
+				</div>
+
+				<div className="mb-4 flex gap-2">
+					<label className="w-[10rem]" htmlFor="lastName">
+						Last Name:
+					</label>
+					<input
+						value={formInfo.fields.find(m => m.idCode === 'lastName')?.value}
+						type="text"
+						id="lastName"
+					/>
+				</div>
+
+				<div className="mb-4 flex gap-2">
+					<label className="w-[10rem]" htmlFor="age">
+						Age:
+					</label>
+					<input
+						value={formInfo.fields.find(m => m.idCode === 'age')?.value}
+						type="text"
+						id="age"
+						className="w-12 text-right"
+					/>
+				</div>
+
+				<div className="mt-5 flex justify-end pr-3">
+					<button>Add Member</button>
+				</div>
+			</fieldset>
+		</form>
+	);
+};
